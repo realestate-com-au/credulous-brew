9b1386b342481ac3287640e68f03469e3ae28a5b
Mike Bailey <mike.bailey@rea-group.com>
2014-06-05 13:15:24 +1000

    Simply process of installing the wrapper script
    Install credulous.sh to /usr/local/etc/profile.d/credulous.sh
    and tell user to source it from their .profile

--- credulous.rb
+++ credulous.rb
@@ -3,11 +3,12 @@ require 'formula'
 class Credulous < Formula
   homepage 'https://github.com/realestate-com-au/credulous'
   version '0.2.0'
-  url "https://github.com/realestate-com-au/credulous/releases/download/#{version}/credulous-#{version}.104-osx.tgz"
-  sha1 'aa232809883dee18021e8d4ab54fcd160f522fc7'
+  url "file:///tmp/credulous-0.2.0.104-osx.tgz"
+  sha1 '5b522f3e0d845ff731cd1a1f1751fca4d5d4fbb8'
 
   def install
     bin.install "credulous"
+    (etc/'profile.d').install "credulous.sh"
     bash_completion.install "credulous.bash_completion" => "credulous"
     man1.mkpath
     man1.install "credulous.1"
@@ -15,6 +16,14 @@ class Credulous < Formula
 
   test do
     assert_equal "Credulous version #{version}", `#{bin}/credulous -v`.strip
+  end
+
+  def caveats; <<-EOS.undent
+    Add the following lines to your ~/.bash_profile:
+      if [ -f $(brew --prefix)/etc/profile.d/credulous.sh ]; then
+        . $(brew --prefix)/etc/profile.d/credulous.sh
+      fi
+    EOS
   end
 
 end
